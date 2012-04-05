95b77b1e8e41b0b6312832840a74fffe1aa524e5
David Lathrop <davidlathrop@gmail.com>
2012-04-04 20:10:47 -0400

    Bumping version number for testing.

--- github3/_version.py
+++ github3/_version.py
@@ -1,9 +1,9 @@
-# This is github3 version 0.6.1 (2012-04-02)
+# This is github3 version 0.6.2 (2012-04-04)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = "0.6.1"
-libtool = "6:21"
-hex = 0x000601
-date = "2012-04-02"
-tuple = (0, 6, 1)
-web = "github/0.6.1"
+dotted = "0.6.2"
+libtool = "6:22"
+hex = 0x000602
+date = "2012-04-04"
+tuple = (0, 6, 2)
+web = "github/0.6.2"
