9ad1d918ed59f7988b228d87dff4f563fcd56839
David Lathrop <davidlathrop@gmail.com>
2012-04-19 16:33:50 -0400

    Updating version number.

--- github3/_version.py
+++ github3/_version.py
@@ -1,9 +1,9 @@
-# This is github3 version 0.6.2 (2012-04-04)
+# This is github3 version 0.6.3 (2012-04-19)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = "0.6.2"
-libtool = "6:22"
-hex = 0x000602
-date = "2012-04-04"
-tuple = (0, 6, 2)
-web = "github/0.6.2"
+dotted = "0.6.3"
+libtool = "6:33"
+hex = 0x000603
+date = "2012-04-19"
+tuple = (0, 6, 3)
+web = "github/0.6.3"
