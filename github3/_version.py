f2c24cbfedd17484f1efffbd822b247bb85ad55c
David Lathrop <davidlathrop@gmail.com>
2012-04-27 14:58:59 -0400

    Bumping the version number before the 1.3.0 (github_login) release of testerfy.

--- github3/_version.py
+++ github3/_version.py
@@ -1,9 +1,9 @@
-# This is github3 version 0.6.3 (2012-04-19)
+# This is github3 version 0.6.4 (2012-04-27)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = "0.6.3"
-libtool = "6:33"
-hex = 0x000603
-date = "2012-04-19"
-tuple = (0, 6, 3)
-web = "github/0.6.3"
+dotted = "0.6.4"
+libtool = "6:44"
+hex = 0x000604
+date = "2012-04-27"
+tuple = (0, 6, 4)
+web = "github/0.6.4"
